45057fb10dd113c16c9dfec6ee5a394f6ac7096b
yoyoconnor <111534505+yoyoconnor@users.noreply.github.com>
2023-03-07 21:32:46 -0600

    finished

--- JBrainTetris.java
+++ JBrainTetris.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.util.ArrayList;
+
 /*
  * public static final int ROTATE = 0;
     public static final int LEFT = 1;
@@ -22,12 +24,15 @@ public class JBrainTetris extends JTetris
     private Brain currbrain;
     private JButton brainEnabler;
     private boolean brainEnabled=false;
+    private Move bestMove;
     JBrainTetris(int width, int height)
     {super(width,height);
-    brainList= new JComboBox(BrainFactory.createBrains().toArray());
+    ArrayList<Brain> LOB=BrainFactory.createBrains();
+    brainList= new JComboBox(LOB.toArray());
     brainEnabler=new JButton("Enable Brain");
     brainList.addActionListener(new brainTypeListener());
     brainEnabler.addActionListener(new enableBrainButton());
+    currbrain=LOB.get(0);
     }
     @Override
     public Container createControlPanel()
@@ -42,14 +47,29 @@ public class JBrainTetris extends JTetris
     @Override
     public Piece pickNextPiece()
     {
-        return currbrain.bestMove(board,super.pickNextPiece(),HEIGHT+TOP_SPACE).getPiece();
-
+        Piece peez=super.pickNextPiece();
+        bestMove=currbrain.bestMove(board,peez,HEIGHT+TOP_SPACE);
+        return peez;
     }
+
     @Override 
     public void tick(int VERB){
-        
+        if(!brainEnabled){super.tick(VERB); return;}
+        if(currentPiece.getSkirt()!=bestMove.getPiece().getSkirt())
+        {
+            super.tick(ROTATE);
+        }
+        if(currentX>bestMove.getX())
+        {
+            super.tick(LEFT);
+        }
+        if(currentX<bestMove.getX())
+        {
+            super.tick(RIGHT);
+        }
+        super.tick(VERB);
     }
-    
+
     
     private class brainTypeListener implements ActionListener
     {
